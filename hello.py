951b5e7faa22990063b2719988e2a6e3cefda209
Sinan Mehmedov <sinanat@abv.bg>
2022-03-09 14:01:41 +0000

    09032022 backup

--- hello.py
+++ hello.py
@@ -5,7 +5,7 @@ from wtforms.validators import DataRequired
 from datetime import datetime
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
-
+from werkzeug.security import generate_password_hash, check_password_hash
 
 
 # # Types of forms elements
@@ -43,6 +43,20 @@ class Users(db.Model):
     favorite_color = db.Column(db.String(120))
     date_added = db.Column(db.DateTime, default=datetime.utcnow)
 
+    # Do some password stuff
+    password_hash = db.Column(db.String(128))
+
+    @property
+    def password(self):
+        raise AttributeError('Password is not a readable attribute!')
+
+    @password.setter
+    def password(self, password):
+        self.passowrd_hash = generate_password_hash(password)
+    
+    def verify_password(self, password):
+        return check_password_hash(self.password_hash, password)
+
     # Create a string.
     def __repr__(self):
         return '<Name %r>'
@@ -65,6 +79,25 @@ class NamerForm(FlaskForm):
     name = StringField("What's your name?", validators=[DataRequired()])
     submit = SubmitField("Submit")
 
+# Delete Database Record
+@app.route('/delete/<int:id>')
+def delete(id):
+    user_to_delete = Users.query.get_or_404(id)
+    name = None
+    form = UserForm()
+
+    try:
+        db.session.delete(user_to_delete)
+        db.session.commit()
+        flash("User deleted successfully")
+
+        our_users = Users.query.order_by(Users.date_added)
+        return render_template('add_user.html',
+        form=form, name=name, our_users=our_users)
+    except:
+        flash("There was a problem deleting the user!")
+        return render_template('add_user.html',
+        form=form, name=name, our_users=our_users)
 
 # Update Database Record.
 @app.route('/update/<int:id>', methods=['GET', 'POST'])
@@ -84,7 +117,8 @@ def update(id):
             flash('Error! There was a problem!')
             return render_template('update.html', form=form, name_to_update=name_to_update)
     else:
-        return render_template('update.html', form=form, name_to_update=name_to_update)
+        return render_template('update.html',
+            form=form, name_to_update=name_to_update, id = id)
 
 
 # Route for user data input.
